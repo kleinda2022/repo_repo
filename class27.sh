16ae23ca48794e90a20f23092b433aa59ed7a34e
kleinda <happir11@gmail.com>
2022-02-22 03:42:46 +0000

    good to practice

---
+++ class27.sh
@@ -0,0 +1,4 @@
+Nelly Tom Alice Ben will be comming for my birthday
+we are fine
+Kiss Diane Linda are my favarite
+
--- test.py
+++ test.py
@@ -1,4 +1,7 @@
- my name is kyssselle
+my name is kyssselle
 and i like It
 i am from comeroun
 i like oranges
+how much is you bag
+where are you doing
+can i come over to visit?
